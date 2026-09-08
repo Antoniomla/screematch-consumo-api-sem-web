26f61e6328659a1f94ee1764832852b845b2b96d
Antonio Carlos.dev <antoniocarlosmelo00@gmail.com>
2026-09-08 20:54:49 -0300

    Atualizando aplicação e  criando novos dados como DadosEpisodio.java, DadosTemporada.java

--- src/main/java/com/projeto/screematch/ScreematchApplication.java
+++ src/main/java/com/projeto/screematch/ScreematchApplication.java
@@ -1,11 +1,16 @@
 package com.projeto.screematch;
 
+import com.projeto.screematch.model.DadosEpisodio;
 import com.projeto.screematch.model.DadosSerie;
+import com.projeto.screematch.model.DadosTemporada;
 import com.projeto.screematch.service.ConsumoApi;
 import com.projeto.screematch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreematchApplication implements CommandLineRunner {
@@ -19,11 +24,24 @@ public class ScreematchApplication implements CommandLineRunner {
         var consumoApi = new ConsumoApi();
         var json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&apikey=f8a9c447");
         //System.out.println(json);
-
         //json = consumoApi.obterDados("https://share.google/QdOO2CDkv0AfOyOML");
         System.out.println(json);
+
         ConverteDados converteDados = new ConverteDados();
         DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
         System.out.println(dadosSerie);
+
+        json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&season=1&episode=2&apikey=f8a9c447");
+        DadosEpisodio dadosEpisodio = converteDados.obterDados(json,DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i <= dadosSerie.totalTemporadas(); i++){
+            json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&season=" + i +"&apikey=f8a9c447");
+            DadosTemporada dadosTemporada = converteDados.obterDados(json,DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
     }
 }
